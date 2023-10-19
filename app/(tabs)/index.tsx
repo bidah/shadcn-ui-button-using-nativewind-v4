42d62279dc97a2e3fc164c8455c023b5c113d86c
rofi <rfigueroa@truelogic.io>
2023-10-19 15:02:45 -0300

    wip

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,6 +18,7 @@ const App = () => {
   return (
     <View className="bg-background flex-1 p-5">
       <Text className="text-primary text-5xl">hello</Text>
+      <Button>hey</Button>
     </View>
     // <View
     //   className="flex-1 items-center justify-center bg-red-100 dark:bg-red-500"
@@ -85,13 +86,48 @@ const button = cva("button", {
   },
 });
 
-interface ButtonProps extends PressableProps, VariantProps<typeof button> {}
+const buttonVariants = cva(
+  "inline-flex items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
+  {
+    variants: {
+      variant: {
+        default: "bg-primary text-primary-foreground hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground hover:bg-destructive/90",
+        outline:
+          "border border-input hover:bg-accent hover:text-accent-foreground",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+        ghost: "hover:bg-accent hover:text-accent-foreground",
+        link: "underline-offset-4 hover:underline text-primary",
+      },
+      size: {
+        default: "h-10 py-2 px-4",
+        sm: "h-9 px-3 rounded-md",
+        lg: "h-11 px-8 rounded-md",
+        icon: "h-10 w-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+interface ButtonProps
+  extends PressableProps,
+    VariantProps<typeof buttonVariants> {}
 
 const Button: React.FC<ButtonProps> = ({
   className,
   intent,
   size,
   ...props
-}) => <Pressable className={button({ intent, size, className })} {...props} />;
+}) => (
+  <Pressable className={buttonVariants({ intent, size, className })} {...props}>
+    <Text className="text-primary">{props.children}</Text>
+  </Pressable>
+);
 
 export default App;
