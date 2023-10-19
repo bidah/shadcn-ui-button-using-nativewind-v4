76c874adddb08d34fd4018959a8bf49cbf6214d9
rofi <rfigueroa@truelogic.io>
2023-10-19 14:07:35 -0300

    with opacity

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,18 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     colors: {
+      primary: "rgb(var(--color-primary) / .2)",
       brand: "var(--my-brand-color)",
     },
   },
   plugins: [
     // Set a default value on the `:root` element
-    ({ addBase }) => addBase({ ":root": { "--my-brand-color": "red" } }),
+    ({ addBase }) =>
+      addBase({
+        ":root": {
+          "--my-brand-color": "red",
+          "--color-primary": "33 134 229",
+        },
+      }),
   ],
 };
