7771773a313d7fb2da4ffb6f7e1a6e727017749f
rofi <rfigueroa@truelogic.io>
2023-11-03 16:29:37 -0300

    x

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,15 +3,6 @@ import { vars, useColorScheme } from "nativewind";
 import { Pressable, Text, View, PressableProps } from "react-native";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
-import React from "react";
-
-const TabOneScreen = () => {
-  return (
-    <View className="bg-background flex-1 p-5">
-      <Button variant={"destructive"}>hello world</Button>
-    </View>
-  );
-};
 
 const variants = [
   "flex-row items-center justify-center rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:opacity-50 disabled:pointer-events-none ring-offset-background",
@@ -52,21 +43,12 @@ interface ButtonProps
   extends PressableProps,
     VariantProps<typeof buttonVariants> {}
 
-const Button: React.FC<ButtonProps> = ({
-  className,
-  variant,
-  intent,
-  size,
-  ...props
-}) => {
+const Button: React.FC<ButtonProps> = ({ className, ...props }) => {
   return (
-    <Pressable
-      className={cn(buttonVariants({ variant, size, className }))}
-      {...props}
-    >
-      <Text className={variants[1][`${variant}Text`]}>{props.children}</Text>
+    <Pressable>
+      <Text>hey hey hey</Text>
     </Pressable>
   );
 };
 
-export default TabOneScreen;
+export default Button;
